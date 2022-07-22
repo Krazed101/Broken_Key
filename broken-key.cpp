e84e8ca9c73e6e5faaf714ee4031b5aa6463fb83
Grant Ellis Ogden <grant101.ogden@gmail.com>
2022-07-21 21:44:01 -0500

    Completed basic lexxer for intro stage. Also set up vscode debugging and compilation.

---
+++ broken-key.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<list>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<sstream>
+
+using namespace std;
+
+vector<string> lex(string fileName);
+
+int main(int argc, char const *argv[])
+{
+    vector<string> lexxer;
+
+    if(argc != 2)
+    {
+        cout << "USAGE:\n\tbroken-key.exe <File-To-Compile>" << '\n';
+        return 1;
+    }
+    cout << "Herro!" << endl;
+    lexxer = lex(argv[1]);
+
+    if(lexxer.empty())
+    {
+        cout << "Uh-Oh Lex function failed" << '\n';
+    }
+    else
+    {
+        vector<string>::iterator it;
+        for (it = lexxer.begin(); it != lexxer.end(); ++it)
+        {
+            cout << *it << '\n';
+        }
+    }
+
+    return 0;
+}
+
+vector<string> lex(string fileName)
+{
+    vector<string> ret;
+    string word;
+    size_t location = string::npos;
+    ifstream fileStream (fileName);
+    if(fileStream.is_open())
+    {
+        while(fileStream >> word)
+        {
+            location = word.find_first_of("(){};");
+            do
+            {
+                
+                if( location != string::npos)
+                {
+                    if(location == 0)
+                    {
+                        ret.push_back(word.substr(location,1));
+                        word = word.substr(location+1);
+                    }
+                    else
+                    {
+                        ret.push_back(word.substr(0,location));
+                        ret.push_back(word.substr(location,1));
+                        word = word.substr(location+1);
+                    }
+                }
+                else
+                {
+                    ret.push_back(word);
+                }
+                location = word.find_first_of("(){};");
+            }while(string::npos != location);
+        }
+        fileStream.close();
+    }
+    else
+        cout << "Unable to open file" << '\n';
+
+    return ret;
+}
